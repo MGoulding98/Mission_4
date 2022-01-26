b32c5cbc7ca1b0c0561508a2176f0b22a3cf2844
Gouldking <59857002+Gouldking@users.noreply.github.com>
2022-01-25 17:35:55 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission_4.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieApplicationContext _MovieContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieApplicationContext x)
+        {
+            _logger = logger;
+            _MovieContext = x;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm(MovieModel m)
+        {
+            _MovieContext.Add(m);
+            _MovieContext.SaveChanges();
+
+            return View("Confirmation");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/MovieApplicationContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_4.Models
+{
+    public class MovieApplicationContext : DbContext
+    {
+        // Constructor
+        public MovieApplicationContext (DbContextOptions<MovieApplicationContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<MovieModel> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieModel>().HasData(
+
+               new MovieModel
+               {
+                   MovieID = 1,
+                   Category = "Dark Comedy",
+                   Title = "Trainspotting",
+                   Year = 1996,
+                   Director = "Danny Boyle",
+                   Rating = "R",
+                   Edited = false,
+                   LentTo = "Matt Goulding",
+                   Notes = "Best movie ever!"
+               },
+
+               new MovieModel
+               {
+                   MovieID = 2,
+                   Category = "Thriller",
+                   Title = "Cure",
+                   Year = 1997,
+                   Director = "Kiyoshi Kurosawa",
+                   Rating = "R",
+                   Edited = false,
+                   LentTo = "Matt Goulding",
+                   Notes = "This one melts your brain."
+               },
+
+               new MovieModel
+               {
+                   MovieID = 3,
+                   Category = "Romantic Comedy",
+                   Title = "A Summer's Tale",
+                   Year = 1996,
+                   Director = "Éric Rohmer",
+                   Rating = "G",
+                   Edited = false,
+                   LentTo = "Matt Goulding",
+                   Notes = "My favorite RomCom."
+               }
+            );
+        }
+    }
+}
